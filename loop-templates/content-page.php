613c68d6c1017eccffc213f437bb4187f631e0e4
shilkomariya <shilkomariya@gmail.com>
2022-05-06 13:05:16 +0300

    13-00

--- loop-templates/content-page.php
+++ loop-templates/content-page.php
@@ -15,22 +15,24 @@ defined( 'ABSPATH' ) || exit;
 				<?php
 				get_template_part( 'template-parts/sidebar', 'menu' );
 				$sidebar_banners = fw_get_db_post_option( get_the_ID(), 'banners' );
-				var_dump( $sidebar_banners );
 				if ( isset( $sidebar_banners["avards"] ) ) {
 					get_template_part( 'template-parts/sidebar', 'awards' );
 				}
 				if ( isset( $sidebar_banners["schedule-call"] ) ) {
 					get_template_part( 'template-parts/sidebar', 'call' );
 				}
+				if ( isset( $sidebar_banners["take-test"] ) ) {
+					get_template_part( 'template-parts/sidebar', 'test' );
+				}
 				?>
             </div>
             <div class="col-auto main-content">
                 <div class="entry-content">
 					<?php the_content(); ?>
-                </div><!-- .entry-content -->
+                </div>
                 <footer class="entry-footer">
 					<?php edit_post_link( __( 'Edit', 'mrprime' ), '<span class="edit-link">', '</span>' ); ?>
-                </footer><!-- .entry-footer -->
+                </footer>
             </div>
         </div>
     </article><!-- #post-## -->
