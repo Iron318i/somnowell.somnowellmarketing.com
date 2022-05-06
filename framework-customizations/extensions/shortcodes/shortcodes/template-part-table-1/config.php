2ce105738342bf020983978209713814f8db9f2c
shilkomariya <shilkomariya@gmail.com>
2022-05-06 15:11:58 +0300

    15-10

---
+++ framework-customizations/extensions/shortcodes/shortcodes/template-part-table-1/config.php
@@ -0,0 +1,13 @@
+<?php
+
+if ( ! defined( 'FW' ) ) {
+	die( 'Forbidden' );
+}
+
+$cfg = array();
+
+$cfg['page_builder'] = array(
+	'title'      => __( 'Features & benefits table', 'fw' ),
+	'tab'        => __( 'Template Parts', 'fw' ),
+	'popup_size' => 'medium'
+);
---
+++ framework-customizations/extensions/shortcodes/shortcodes/template-part-table-1/views/view.php
@@ -0,0 +1,128 @@
+<?php
+
+if ( ! defined( 'FW' ) ) {
+	die( 'Forbidden' );
+}
+
+/**
+ * @var array $atts
+ */
+?>
+<h5 class="text-info">COMPARISON TABLE</h5>
+<hr>
+<div class="table-responsive">
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col"></th>
+            <th scope="col" class="text-info">Somnowell Chrome</th>
+            <th scope="col">CPAP</th>
+            <th scope="col">Surgery</th>
+            <th scope="col">Over-the-Counter Plastic Oral Devices</th>
+            <th scope="col">Mid-Range Plastic Oral Devices</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr class="heading">
+            <th colspan="6">Effective</th>
+        </tr>
+        <tr>
+            <td style="width: 10rem;">Medical Supervision</td>
+            <td><span class="check"></span></td>
+            <td>Yes</td>
+            <td>Yes</td>
+            <td>No</td>
+            <td>Yes</td>
+        </tr>
+        <tr>
+            <td>Adjustable</td>
+            <td><span class="check"></span></td>
+            <td>n/a</td>
+            <td>n/a</td>
+            <td>Sometimes</td>
+            <td>Sometimes</td>
+        </tr>
+        <tr class="heading">
+            <th colspan="6">Comfortable</th>
+        </tr>
+        <tr>
+            <td>Comfortable to use</td>
+            <td>Very comfortable</td>
+            <td>Many people cannot tolerate</td>
+            <td>Can be very painful</td>
+            <td>Often not comfortable</td>
+            <td>Often not comfortable</td>
+        </tr>
+        <tr>
+            <td>Hardly noticeable</td>
+            <td><span class="check"></span></td>
+            <td>No</td>
+            <td>Yes</td>
+            <td>Bulky devices are noticeable</td>
+            <td>Bulky devices are noticeable</td>
+        </tr>
+        <tr>
+            <td>Can talk and drink</td>
+            <td><span class="check"></span></td>
+            <td>n/a</td>
+            <td>n/a</td>
+            <td>Sometimes</td>
+            <td>Sometimes</td>
+        </tr>
+        <tr class="heading">
+            <th colspan="6">Durable</th>
+        </tr>
+        <tr>
+            <td>Lifespan</td>
+            <td>10+ years</td>
+            <td>n/a</td>
+            <td>n/a - often the effects wear off</td>
+            <td>6 months</td>
+            <td>12-18 months</td>
+        </tr>
+        <tr>
+            <td>Regular Replacement</td>
+            <td>No</td>
+            <td>No - parts need regular replacement</td>
+            <td>No</td>
+            <td>Yes</td>
+            <td>Yes</td>
+        </tr>
+        <tr>
+            <td>Quickly degrades</td>
+            <td>No</td>
+            <td>n/a</td>
+            <td>n/a</td>
+            <td>Yes</td>
+            <td>Yes</td>
+        </tr>
+        <tr class="heading">
+            <th colspan="6">Hygienic</th>
+        </tr>
+        <tr>
+            <td>Becomes unsightly and smelly</td>
+            <td>No</td>
+            <td>No - requires intensive cleaning</td>
+            <td>n/a</td>
+            <td>Yes</td>
+            <td>Yes</td>
+        </tr>
+        <tr>
+            <td>Easy to clean</td>
+            <td><span class="check"></span></td>
+            <td>n/a</td>
+            <td>n/a</td>
+            <td>No - becomes unsightly and smelly over time</td>
+            <td>No - becomes unsightly and smelly over time</td>
+        </tr>
+        <tr class="heading">
+            <th>Price Guide</th>
+            <th class="text-info">£££</th>
+            <th class="text-info">££££</th>
+            <th class="text-info">££££</th>
+            <th class="text-info">£</th>
+            <th class="text-info">££</th>
+        </tr>
+        </tbody>
+    </table>
+</div>
