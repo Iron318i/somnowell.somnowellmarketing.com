0652d39cafd22d2df7263fcf6e970aba82bda6cd
shilkomariya <shilkomariya@gmail.com>
2022-05-12 13:08:11 +0300

    13-00

--- framework-customizations/extensions/shortcodes/shortcodes/special-heading/options.php
+++ framework-customizations/extensions/shortcodes/shortcodes/special-heading/options.php
@@ -1,75 +1,74 @@
 <?php
 
-if (!defined('FW')) {
-    die('Forbidden');
+if ( ! defined( 'FW' ) ) {
+	die( 'Forbidden' );
 }
 
 $options = array(
-    'title' => array(
-        'type' => 'text',
-        'label' => __('Heading Title', 'fw'),
-        'desc' => __('Write the heading title content', 'fw'),
-    ),
-    'top_subtitle' => array(
-        'type' => 'text',
-        'label' => __('Top Heading Subtitle', 'fw'),
-        'desc' => __('Write the heading subtitle content', 'fw'),
-    ),
-    'subtitle' => array(
-        'type' => 'text',
-        'label' => __('Heading Subtitle', 'fw'),
-        'desc' => __('Write the heading subtitle content', 'fw'),
-    ),
-    'heading' => array(
-        'type' => 'select',
-        'label' => __('Heading Size', 'fw'),
-        'choices' => array(
-            'h1' => 'H1',
-            'h2' => 'H2',
-            'h3' => 'H3',
-            'h4' => 'H4',
-            'h5' => 'H5',
-            'h6' => 'H6',
-        )
-    ),
-    'heading_color' => array(
-        'type' => 'select',
-        'label' => __('Heading Color', 'fw'),
-        'choices' => array(
-            '' => '---',
-            'text-white' => 'White',
-            'text-primary' => 'Primary',
-        )
-    ),
-    'heading_weight' => array(
-        'type' => 'select',
-        'label' => __('Font weight', 'fw'),
-        'choices' => array(
-            '' => 'Bold',
-            'fw-black' => 'Black',
-        )
-    ),
-    'margin_bottom' => array(
-        'label' => __('Margin Bottom', 'fw'),
-        'type' => 'slider',
-        'value' => 1,
-        'properties' => array(
-            'min' => 0,
-            'max' => 5,
-            'step' => 1, // Set slider step. Always > 0. Could be fractional.
-        ),
-    ),
-    'centered' => array(
-        'type' => 'switch',
-        'label' => __('Centered', 'fw'),
-    ),
-    'ttu' => array(
-        'type' => 'switch',
-        'label' => __('Uppercase', 'fw'),
-    ),
-    'mw' => array(
-        'type' => 'text',
-        'label' => __('Max Width', 'fw'),
-        'desc' => __('Enter width in px', 'fw')
-    ),
+	'title'          => array(
+		'type'  => 'text',
+		'label' => __( 'Heading Title', 'fw' ),
+		'desc'  => __( 'Write the heading title content', 'fw' ),
+	),
+	'subtitle'       => array(
+		'type'  => 'text',
+		'label' => __( 'Heading Subtitle', 'fw' ),
+		'desc'  => __( 'Write the heading subtitle content', 'fw' ),
+	),
+	'heading'        => array(
+		'type'    => 'select',
+		'label'   => __( 'Heading Size', 'fw' ),
+		'choices' => array(
+			'h1' => 'H1',
+			'h2' => 'H2',
+			'h3' => 'H3',
+			'h4' => 'H4',
+			'h5' => 'H5',
+			'h6' => 'H6',
+		)
+	),
+	'heading_color'  => array(
+		'type'    => 'select',
+		'label'   => __( 'Heading Color', 'fw' ),
+		'choices' => array(
+			''             => '---',
+			'text-white'   => 'White',
+			'text-primary' => 'Primary',
+		)
+	),
+	'heading_weight' => array(
+		'type'    => 'select',
+		'label'   => __( 'Font weight', 'fw' ),
+		'choices' => array(
+			''         => 'Bold',
+			'fw-black' => 'Black',
+		)
+	),
+	'margin_bottom'  => array(
+		'label'      => __( 'Margin Bottom', 'fw' ),
+		'type'       => 'slider',
+		'value'      => 1,
+		'properties' => array(
+			'min'  => 0,
+			'max'  => 5,
+			'step' => 1, // Set slider step. Always > 0. Could be fractional.
+		),
+	),
+	'line'           => array(
+		'type'  => 'switch',
+		'label' => __( 'Line', 'fw' ),
+	),
+	'centered'       => array(
+		'type'  => 'switch',
+		'label' => __( 'Centered', 'fw' ),
+	),
+	'ttu'            => array(
+		'type'  => 'switch',
+		'label' => __( 'Uppercase', 'fw' ),
+	),
+	'mw'             => array(
+		'type'  => 'text',
+		'label' => __( 'Max Width', 'fw' ),
+		'desc'  => __( 'Enter width in px', 'fw' )
+	),
 );
--- framework-customizations/extensions/shortcodes/shortcodes/special-heading/views/view.php
+++ framework-customizations/extensions/shortcodes/shortcodes/special-heading/views/view.php
@@ -1,69 +1,68 @@
 <?php
-if (!defined('FW'))
-    die('Forbidden');
+if ( ! defined( 'FW' ) ) {
+	die( 'Forbidden' );
+}
 /**
  * @var $atts
  */
-$class = ($atts['ttu']) ? ' text-uppercase' : '';
-$class .= ($atts['subtitle']) ? ' mb-0' : '';
-$wrapper_class = ($atts['heading_color']) ? ' ' . $atts['heading_color'] : '';
-$class .= ($atts['heading_weight']) ? ' ' . $atts['heading_weight'] : '';
-if (empty($atts['subtitle'])):
-    switch ($atts['margin_bottom']) {
-        case 0:
-            $class .= ' mb-0';
-            break;
-        case 1:
-            break;
-        case 2:
-            $class .= ' mb-1 mb-md-2';
-            break;
-        case 3:
-            $class .= ' mb-2 mb-md-3';
-            break;
-        case 4:
-            $class .= ' mb-2 mb-md-4';
-            break;
-        case 5:
-            $class .= ' mb-3 mb-md-5';
-            break;
-    }
+$class         = ( $atts['ttu'] ) ? ' text-uppercase' : '';
+$class         .= ( $atts['subtitle'] ) ? ' mb-0' : '';
+$wrapper_class = ( $atts['heading_color'] ) ? ' ' . $atts['heading_color'] : '';
+$wrapper_class .= ( $atts['line'] ) ? ' line' : '';
+$class         .= ( $atts['heading_weight'] ) ? ' ' . $atts['heading_weight'] : '';
+if ( empty( $atts['subtitle'] ) ):
+	switch ( $atts['margin_bottom'] ) {
+		case 0:
+			$class .= ' mb-0';
+			break;
+		case 1:
+			break;
+		case 2:
+			$class .= ' mb-1 mb-md-2';
+			break;
+		case 3:
+			$class .= ' mb-2 mb-md-3';
+			break;
+		case 4:
+			$class .= ' mb-2 mb-md-4';
+			break;
+		case 5:
+			$class .= ' mb-3 mb-md-5';
+			break;
+	}
 endif;
 $sub_class = '';
-if (!empty($atts['subtitle'])):
-    switch ($atts['margin_bottom']) {
-        case 0:
-            $sub_class .= ' mb-0';
-            break;
-        case 1:
-            $sub_class .= ' mb-1';
-            break;
-        case 2:
-            $sub_class .= ' mb-1 mb-md-2';
-            break;
-        case 3:
-            $sub_class .= ' mb-2 mb-md-3';
-            break;
-        case 4:
-            $sub_class .= ' mb-2 mb-md-4';
-            break;
-        case 5:
-            $sub_class .= ' mb-3 mb-md-5';
-            break;
-    }
+if ( ! empty( $atts['subtitle'] ) ):
+	switch ( $atts['margin_bottom'] ) {
+		case 0:
+			$sub_class .= ' mb-0';
+			break;
+		case 1:
+			$sub_class .= ' mb-1';
+			break;
+		case 2:
+			$sub_class .= ' mb-1 mb-md-2';
+			break;
+		case 3:
+			$sub_class .= ' mb-2 mb-md-3';
+			break;
+		case 4:
+			$sub_class .= ' mb-2 mb-md-4';
+			break;
+		case 5:
+			$sub_class .= ' mb-3 mb-md-5';
+			break;
+	}
 endif;
 $style = '';
-if ($atts['mw'] > 0) {
-    $style = 'style = "max-width: ' . $atts['mw'] / 18 . 'rem;"';
+if ( $atts['mw'] > 0 ) {
+	$style = 'style = "max-width: ' . $atts['mw'] / 18 . 'rem;"';
 }
 ?>
-<div class="fw-heading fw-heading-<?php echo esc_attr($atts['heading']); ?> <?php echo !empty($atts['centered']) ? 'text-center' : ''; ?><?php echo $wrapper_class ?>" <?= $style ?>>
-    <?php if (!empty($atts['top_subtitle'])): ?>
-        <div class="fw-special-subtitle mb-1"><?php echo $atts['top_subtitle']; ?></div>
-    <?php endif; ?>
-    <?php $heading = "<{$atts['heading']} class='fw-special-title{$class}'>{$atts['title']}</{$atts['heading']}>"; ?>
-    <?php echo $heading; ?>
-    <?php if (!empty($atts['subtitle'])): ?>
+<div class="fw-heading fw-heading-<?php echo esc_attr( $atts['heading'] ); ?> <?php echo ! empty( $atts['centered'] ) ? 'text-center' : ''; ?><?php echo $wrapper_class ?>" <?= $style ?>>
+	<?php $heading = "<{$atts['heading']} class='fw-special-title{$class}'>{$atts['title']}</{$atts['heading']}>"; ?>
+	<?php echo $heading; ?>
+	<?php if ( ! empty( $atts['subtitle'] ) ): ?>
         <div class="fw-special-subtitle<?php echo $sub_class ?>"><?php echo $atts['subtitle']; ?></div>
-    <?php endif; ?>
+	<?php endif; ?>
 </div>
