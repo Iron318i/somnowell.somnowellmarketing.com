fd96ae9abd34983c8166d02693629ad7d765b650
shilkomariya <shilkomariya@gmail.com>
2022-05-13 11:36:12 +0300

    11-35

--- framework-customizations/extensions/shortcodes/shortcodes/button/views/view.php
+++ framework-customizations/extensions/shortcodes/shortcodes/button/views/view.php
@@ -1,9 +1,13 @@
-<?php if (!defined('FW')) die('Forbidden'); ?>
-<?php $color_class = !empty($atts['color']) ? "btn-{$atts['color']}" : ''; ?>
-<?php $size_class = !empty($atts['size']) ? " {$atts['size']}" : ''; ?>
+<?php if ( ! defined( 'FW' ) ) {
+	die( 'Forbidden' );
+}
+$class = ! empty( $atts['color'] ) ? "btn-{$atts['color']}" : '';
+$class .= ! empty( $atts['size'] ) ? " {$atts['size']}" : '';
+$class .= ( $atts['full'] ) ? ' d-block' : '';
+?>
 
 <div class="btn-wrp">
-    <a href="<?php echo do_shortcode($atts['link']) ?>" target="<?php echo esc_attr($atts['target']) ?>" class="btn <?php echo esc_attr($color_class); ?><?php echo $size_class; ?>">
-	<span><?php echo $atts['label']; ?></span>
+    <a href="<?php echo do_shortcode( $atts['link'] ) ?>" target="<?php echo esc_attr( $atts['target'] ) ?>" class="btn <?php echo esc_attr( $class ); ?>">
+        <span><?php echo $atts['label']; ?></span>
     </a>
 </div>
