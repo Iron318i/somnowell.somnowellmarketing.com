fcf9d8816f0b4e88f112b38991c7b50eb0d2cd3c
shilkomariya <shilkomariya@gmail.com>
2022-05-18 14:11:56 +0300

    14-10

---
+++ framework-customizations/extensions/shortcodes/shortcodes/table-2/config.php
@@ -0,0 +1,13 @@
+<?php
+
+if ( ! defined( 'FW' ) ) {
+	die( 'Forbidden' );
+}
+
+$cfg = array();
+
+$cfg['page_builder'] = array(
+	'title'      => __( 'Promoted Practitioners', 'fw' ),
+	'tab'        => __( 'Tables', 'fw' ),
+	'popup_size' => 'medium'
+);
---
+++ framework-customizations/extensions/shortcodes/shortcodes/table-2/views/view.php
@@ -0,0 +1,102 @@
+<?php
+
+if ( ! defined( 'FW' ) ) {
+	die( 'Forbidden' );
+}
+
+/**
+ * @var array $atts
+ */
+?>
+<h5 class="text-info">Promoted Practitioner Program Choice</h5>
+<hr>
+<div class="table-responsive">
+    <table class="table table-borderless">
+        <thead>
+        <tr>
+            <th scope="col"></th>
+            <th scope="col" class="text-info">Practice Starter
+            </th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr class="heading">
+            <th colspan="2">New Patient Enquiries</th>
+        </tr>
+        <tr>
+            <td>New patient enquiries per month</td>
+            <td><span class="h1">5+</span></td>
+        </tr>
+        <tr class="heading">
+            <th colspan="2">Training</th>
+        </tr>
+        <tr>
+            <td>Clinical Guidance by Prof. Ash - 2 hours CPD <br> <span class="text-info">FREE FOR A STRICTLY LIMITED TIME - SAVE £250!</span></td>
+            <td><span class="check"></span></td>
+        </tr>
+        <tr>
+            <td>Introductory Course by Prof. Ash - 6 hours CPD <br> <span class="text-info">FREE FOR A STRICTLY LIMITED TIME - SAVE £336!</span></td>
+            <td><span class="check"></span></td>
+        </tr>
+        <tr>
+            <td>Advanced Course by Prof. Ash - 6 hours CPD <br> <span class="text-info">FREE FOR A STRICTLY LIMITED TIME - SAVE £336!</span></td>
+            <td><span class="check-no"></span></td>
+        </tr>
+        <tr class="heading">
+            <th colspan="2">Starter Pack</th>
+        </tr>
+        <tr>
+            <td>80 page manual</td>
+            <td><span class="check"></span></td>
+        </tr>
+        <tr>
+            <td>11 posters</td>
+            <td><span class="check"></span></td>
+        </tr>
+        <tr>
+            <td>150 patient information leaflets</td>
+            <td><span class="check"></span></td>
+        </tr>
+        <tr class="heading">
+            <th colspan="2">Somnowell.com Website</th>
+        </tr>
+        <tr>
+            <td>Listing on website</td>
+            <td><span class="check"></span></td>
+        </tr>
+        <tr>
+            <td>Dedicated profile page</td>
+            <td><span class="check"></span></td>
+        </tr>
+        <tr>
+            <td>Dedicated contact form</td>
+            <td><span class="check"></span></td>
+        </tr>
+        <tr class="heading">
+            <th colspan="2">Plus</th>
+        </tr>
+        <tr>
+            <td>Practitioner Registration</td>
+            <td><span class="check"></span></td>
+        </tr>
+        <tr>
+            <td>No minimum term</td>
+            <td><span class="check"></span></td>
+        </tr>
+        <tr class="heading">
+            <th colspan="2">New Patient Enquiries</th>
+        </tr>
+        <tr>
+            <td>New patient enquiries per month</td>
+            <td><span class="h1">5+</span></td>
+        </tr>
+        <tr class="heading">
+            <th colspan="2">Price</th>
+        </tr>
+        <tr>
+            <td>No obligation, cancel anytime</td>
+            <td>£50 per month (plus VAT)</td>
+        </tr>
+        </tbody>
+    </table>
+</div>
