a9467ecdee771daf282ada19452b60433cd0639a
shilkomariya <shilkomariya@gmail.com>
2022-05-23 14:22:05 +0300

    14-20

--- framework-customizations/extensions/shortcodes/shortcodes/useful-links/views/view.php
+++ framework-customizations/extensions/shortcodes/shortcodes/useful-links/views/view.php
@@ -9,21 +9,30 @@ if ( ! defined( 'FW' ) ) {
 
 $id = fw_unique_increment();
 ?>
-<div class="faq mb-2">
-    <div class="accordion" id="accordionFaq-<?php echo $id; ?>">
-		<?php foreach ( $atts['faq'] as $key => $value ) { ?>
+<div class="useful-links mb-2">
+    <div class="accordion" id="accordionLinks-<?php echo $id; ?>">
+		<?php foreach ( $atts['links'] as $key => $value ) { ?>
             <div class="accordion-item">
                 <h5 class="accordion-header" id="flush-heading-<?php echo $key ?>-<?php echo $id; ?>">
-                    <button class="accordion-button collapsed" type="button"
+                    <button class="accordion-button<?php if ( ! $key )
+						echo ' collapsed' ?>" type="button"
                             data-bs-toggle="collapse" data-bs-target="#flush-collapse-<?php echo $key ?>-<?php echo $id; ?>"
                             aria-expanded="false" aria-controls="flush-collapse-<?php echo $key ?>-<?php echo $id; ?>">
-						<?php echo $value['question'] ?>
+                        <span><?php echo $value['cat'] ?></span>
                     </button>
                 </h5>
                 <div id="flush-collapse-<?php echo $key ?>-<?php echo $id; ?>"
-                     class="accordion-collapse collapse"
-                     aria-labelledby="flush-heading-<?php echo $key ?>-<?php echo $id; ?>" data-bs-parent="#accordionFaq-<?php echo $id; ?>">
-                    <div class="accordion-body"><?php echo $value['answer'] ?></div>
+                     class="accordion-collapse collapse<?php if ( ! $key )
+					     echo ' show' ?>"
+                     aria-labelledby="flush-heading-<?php echo $key ?>-<?php echo $id; ?>" data-bs-parent="#accordionLinks-<?php echo $id; ?>">
+                    <div class="accordion-body">
+						<?php foreach ( $value['link-items'] as $link ) { ?>
+                            <div class="link">
+                                <div class="name"><?php echo $link['name'] ?></div>
+                                <a href="<?php echo $link['url'] ?>" class="btn btn-sm btn-outline-info" target="_blank">View</a>
+                            </div>
+						<?php } ?>
+                    </div>
                 </div>
             </div>
 		<?php } ?>
